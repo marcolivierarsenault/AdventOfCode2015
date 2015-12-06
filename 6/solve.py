5a616d6d3451c596626faacc220d823ec2511b9b
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2015-12-06 11:08:19 -0500

    optimized with numpy based on https://github.com/jjhelmus/adventofcode

--- 6/solve.py
+++ 6/solve.py
@@ -3,8 +3,7 @@ import numpy as np
 
 a = np.zeros((1000,1000), dtype=bool)
 b = np.zeros((1000,1000), dtype=int)
-total = 0
-total2 = 0
+
 with open("input.txt") as textFile:
 	for line in textFile:
 		SuperPowerRegexParty = re.search(r'^(.*)( )(\d{1,3})(,)(\d{1,3})( through )(\d{1,3})(,)(\d{1,3})$',line.strip())
@@ -13,33 +12,15 @@ with open("input.txt") as textFile:
 		y1 = int(SuperPowerRegexParty.group(5))
 		x2 = int(SuperPowerRegexParty.group(7))
 		y2 = int(SuperPowerRegexParty.group(9))
-		if (command=="toggle"):
-			for x in range(x1,x2+1):
-				for y in range(y1,y2+1):
-					if a[x,y]:
-						a[x,y] = 0
-						total -= 1
-					else:
-						a[x,y] = 1
-						total += 1
-					b[x,y]+=2
-					total2+=2
-		elif (command=="turn off"):
-			for x in range(x1,x2+1):
-				for y in range(y1,y2+1):
-					if a[x,y]:
-						total -= 1
-					a[x,y] = 0
-					if b[x,y] != 0:
-						b[x,y]-=1
-						total2-=1
-		elif (command=="turn on"):
-			for x in range(x1,x2+1):
-				for y in range(y1,y2+1):
-					if not a[x,y]:
-						total += 1
-					a[x,y] = 1
-					b[x,y]+=1
-					total2+=1
-print "Q1: There is ", total, " lights open"
-print "Q2: Total lumens for theses lights is ", total2
+		if (command=="toggle"):			
+			a[x1:x2+1, y1:y2+1] = np.logical_not(a[x1:x2+1, y1:y2+1])
+			b[x1:x2+1, y1:y2+1] += 2
+		elif (command=="turn off"):			
+			a[x1:x2+1, y1:y2+1] = 0
+			b[x1:x2+1, y1:y2+1] -= 1
+			b[b < 0] = 0
+		elif (command=="turn on"):			
+			a[x1:x2+1, y1:y2+1] = 1
+			b[x1:x2+1, y1:y2+1] += 1
+print "Q1: There is ", np.sum(a), " lights open"
+print "Q2: Total lumens for theses lights is ", np.sum(b)
