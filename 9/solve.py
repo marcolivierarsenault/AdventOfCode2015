3544cc394eb3b68a49a9c33bdfa79db095570695
Marc-Olivier Arsenault <marc-olivier.arsenault@ericsson.com>
2015-12-09 10:03:02 -0500

    9 working

--- 9/solve.py
+++ 9/solve.py
@@ -4,32 +4,35 @@ table = []
 endTable = []
 
 with open("input.txt") as textFile:
-	for i,line in enumerate(textFile):
+	for line in textFile:
 		words = line.strip().split(" ")
 		table.append([])
-		table[i].append(words[4])
-		table[i].append(words[0])
-		table[i].append(words[2])
+		table[len(table)-1].append(words[4])
+		table[len(table)-1].append(words[0])
+		table[len(table)-1].append(words[2])
+		table.append([])
+		table[len(table)-1].append(words[4])
+		table[len(table)-1].append(words[2])
+		table[len(table)-1].append(words[0])
 endTable = copy.deepcopy(table)
 
 
 over = False
 while not over:
 	newTable = []
-	print len(endTable)
 	for entry in endTable:
 		for new in table:
 			if entry[len(entry)-1] == new[1]:
-				newTable.append([])
-				for current in entry:
-					newTable[len(newTable)-1].append(current)
-				newTable[len(newTable)-1][0] += new[0]
-				newTable[len(newTable)-1].append(new[2])
-	print len(newTable)
+				if new[2] not in entry:
+					newTable.append([])
+					for current in entry:
+						newTable[len(newTable)-1].append(current)
+					newTable[len(newTable)-1][0] = int(newTable[len(newTable)-1][0])+int(new[0])
+					newTable[len(newTable)-1].append(new[2])
 	if len(newTable) == 0:
 		over = True
 	else:
 		endTable = copy.deepcopy(newTable)
 
-
-print len(endTable)
+print "The shortest path is ", min(endTable)[0]
+print "The longest path is ", max(endTable)[0]
