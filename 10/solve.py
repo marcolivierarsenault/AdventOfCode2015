6738fc40d1c6695226e88e8cd2354a7639120a11
Marc-Olivier Arsenault <marc-olivier.arsenault@ericsson.com>
2015-12-10 08:44:23 -0500

    nicer output

--- 10/solve.py
+++ 10/solve.py
@@ -1,12 +1,11 @@
 import re
-
 line = "3113322113"
-
 for n in range(50):
-    print n
     newline = ""
     for match in re.findall(r'((\d)\2*)', line):
         newline += str(len(match[0])) + match[1]
     line = newline
-
-print len(line)
+    if n == 40-1:
+        print "The string length for 40 rep is: ", len(line)
+    elif n == 50-1:
+        print "The string length for 50 rep is: ", len(line)
